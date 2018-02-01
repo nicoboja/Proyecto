3e2695d4ef14a8e76989a36ae6f281b80acbf278
nicoboja <nico.boja@gmail.com>
2018-02-01 19:26:53 -0300

    .

---
+++ src/util/AppDataException.java
@@ -0,0 +1,28 @@
+package util;
+
+public class AppDataException extends Exception{
+	private Throwable innerException;
+	private String message;
+	
+	
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+	public AppDataException(Throwable e, String message){
+		this.innerException=e;
+		this.setMessage(message);
+	}
+
+
+	public AppDataException(String e) {
+		this.setMessage(message);
+	}
+
+}
