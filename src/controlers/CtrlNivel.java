ed52fbd02512c5f07e8dea696c62fc14db5de2e8
nicoboja <nico.boja@gmail.com>
2018-02-13 19:30:19 -0300

    .

---
+++ src/controlers/CtrlNivel.java
@@ -0,0 +1,18 @@
+package controlers;
+
+import java.util.ArrayList;
+
+import data.DataNivel;
+import entity.Nivel;
+
+
+public class CtrlNivel {
+	private DataNivel dataN = new DataNivel();
+	
+	public ArrayList<Nivel> getAll() {
+		
+		return null;
+		
+	}
+
+}
